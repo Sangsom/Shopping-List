fef8eeb348166f8e2730d5d2aa1022207988e0c0
Rinalds Domanovs <rinalds.domanovs@gmail.com>
2019-06-14 14:11:43 +0300

    Initial Commit

---
+++ Shopping List/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Shopping List
+//
+//  Created by Rinalds Domanovs on 14/06/2019.
+//  Copyright © 2019 Rinalds Domanovs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
